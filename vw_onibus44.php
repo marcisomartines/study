3c1951e938ffc0fc855076014483a5de8d912bdb
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-08-31 22:24:42 -0400

    layout

--- vw_onibus44.php
+++ vw_onibus44.php
@@ -1,74 +1,75 @@
 <table>
 	<tr>
-		<td></td>
-		<td></td>
-		<td rowspan="12"></td>
-		<td></td>
-		<td></td>
+		<td>1</td>
+		<td>2</td>
+		<td>CORREDOR</td>
+		<td>4</td>
+		<td>3</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>5</td>
+		<td>6</td>
+		<td rowspan="10">
+		<td colspan="2">ESCADA</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>9</td>
+		<td>10</td>
+		<td>8</td>
+		<td>7</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>13</td>
+		<td>14</td>
+		<td>12</td>
+		<td>11</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>17</td>
+		<td>18</td>
+		<td>16</td>
+		<td>15</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>21</td>
+		<td>22</td>
+		<td>20</td>
+		<td>19</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>25</td>
+		<td>26</td>
+		<td>24</td>
+		<td>23</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>29</td>
+		<td>30</td>
+		<td>28</td>
+		<td>27</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>33</td>
+		<td>34</td>
+		<td>32</td>
+		<td>31</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>37</td>
+		<td>38</td>
+		<td>36</td>
+		<td>35</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td>41</td>
+		<td>42</td>
+		<td>40</td>
+		<td>39</td>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
+		<td>43</td>
+		<td>44</td>
+		<td>GELADEIRA</td>
 		<td colspan="2">Banheiro</td>
 	</tr>
 </table>
