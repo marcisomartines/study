b32547d5ba96b2ffb72cb5504625fab9083653f2
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-09-06 23:04:07 -0400

    listar() sem parametro

--- func/func.php
+++ func/func.php
@@ -4,5 +4,12 @@ function teste(){
 }
 
 function lista(){
-	
+$servidor="localhost";
+$banco="db_sispantur";
+$usuario="root";
+$senha="123456";
+
+$db= new mysqli($servidor,$usuario,$senha,$banco);
+$query=$db->query("SELECT * FROM tb_clients");
+return $query;	
 }
--- func/index.php
+++ func/index.php
@@ -1,4 +1,8 @@
 <?php
 include 'func.php';
 teste();
-?>
+echo "<br>------------------------------<br>";
+$teste=lista();
+while($row=$teste->fetch_object()){
+	echo $row->nome,"<br>";
+}
