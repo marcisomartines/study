817565222c75d3231393866625d4495467a37b53
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-09-07 11:17:39 -0400

    testes

--- func/func.php
+++ func/func.php
@@ -4,14 +4,14 @@ function teste(){
 }
 
 function lista(){
-$servidor="localhost";
-$banco="db_sispantur";
-$usuario="root";
-$senha="123456";
+	$servidor="localhost";
+	$banco="db_sispantur";
+	$usuario="root";
+	$senha="123456";
 
-$db= new mysqli($servidor,$usuario,$senha,$banco);
-$query=$db->query("SELECT * FROM tb_clients");
-return $query;	
+	$db= new mysqli($servidor,$usuario,$senha,$banco);
+	$query=$db->query("SELECT * FROM tb_clients");
+	return $query;	
 }
 
 function listarParametro($parametro){
@@ -23,4 +23,17 @@ function listarParametro($parametro){
 	$sql="SELECT * FROM ".$parametro;
 	$query=$db->query($sql);
 	return $query;
+}
+
+function criarTabela($dados){
+	echo '<table border="1">';
+	reset($dados);//aponta para o inicio do  array
+	$value=current($dados);
+	while($value){
+		echo '<tr>';
+		echo '<td>'.$value.'</td>';
+		echo '</tr>';
+		$value=next($dados);
+	}
+	echo '</table>';
 }
--- func/index.php
+++ func/index.php
@@ -5,8 +5,10 @@ echo "<br>------------------------------<br>";
 /*$teste=lista();
 while($row=$teste->fetch_object()){
 	echo $row->nome,"<br>";
-}*/
+}
 $teste=listarParametro('tb_clients');
 while($row=$teste->fetch_object()){
 	echo $row->nome,"<br>";
-}
+}*/
+$teste=array("valor 1","valor 2","valor 3");
+$criarTabela($teste);
