81af0eaa8c4bb2e31d7bbf7334dcbd7064d91517
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-09-06 23:24:55 -0400

    listarParamentro()

--- func/func.php
+++ func/func.php
@@ -13,3 +13,14 @@ $db= new mysqli($servidor,$usuario,$senha,$banco);
 $query=$db->query("SELECT * FROM tb_clients");
 return $query;	
 }
+
+function listarParametro($parametro){
+	$servidor="localhost";
+	$banco="db_sispantur";
+	$usuario="root";
+	$senha="123456";
+	$db=new mysqli($servidor,$usuario,$senha,$banco);
+	$sql="SELECT * FROM ".$parametro;
+	$query=$db->query($sql);
+	return $query;
+}
--- func/index.php
+++ func/index.php
@@ -2,7 +2,11 @@
 include 'func.php';
 teste();
 echo "<br>------------------------------<br>";
-$teste=lista();
+/*$teste=lista();
+while($row=$teste->fetch_object()){
+	echo $row->nome,"<br>";
+}*/
+$teste=listarParametro('tb_clients');
 while($row=$teste->fetch_object()){
 	echo $row->nome,"<br>";
 }
