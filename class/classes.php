cb3798a69690bc39e7938337da4ddb70171faa7d
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-09-07 20:38:05 -0400

    classes

---
+++ class/classes.php
@@ -0,0 +1,12 @@
+<?php
+class teste{
+	function teste($parametro){
+		echo "Parametro passado: $parametro <br>";
+	}
+
+	var $atributo;
+	function soma($par1, $par2){
+		$this->atributo=$par1+$par2;
+		return $this->atributo;
+	}
+}
---
+++ class/index.php
@@ -0,0 +1,9 @@
+<?php
+include('classes.php');
+
+//$a = new teste('Marciso');
+//$b = new teste('teste');
+//$c = new teste();
+$soma = new teste('Marciso');
+$resultado=$soma->soma(1,1);
+echo $resultado;
