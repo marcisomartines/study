6f10e77ddab2b26f7bf988c081efc476bf12ae6e
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-09-11 15:27:12 -0400

    menu

--- foundation/fun.php
+++ foundation/fun.php
@@ -2,12 +2,42 @@
 <head>
 	<title>Teste com o fundation</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<script src="js/vendor/jquery.js"></script>
 	<script src="js/foundation.min.js"></script>
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/foundation.css">
 	<link rel="stylesheet" href="css/foundation.min.css">
 </head>
 <body>
+	<nav class="top-bar" data-topbar role="navigation">
+		<ul class="title-area">
+			<li class="name">
+				<h1><a href="#">My Site</a></h1>
+			</li>
+			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		</ul>
+
+		<section class="top-bar-section">
+			<!-- Right Nav Section -->
+			<ul class="right">
+				<li class="active"><a href="#">Right Button Active</a></li>
+				<li class="has-dropdown">
+					<a href="#">Right Button Dropdown</a>
+					<ul class="dropdown">
+						<li><a href="#">First link in dropdown</a></li>
+						<li class="active"><a href="#">Active link in dropdown</a></li>
+					</ul>
+				</li>
+			</ul>
+
+			<!-- Left Nav Section -->
+			<ul class="left">
+				<li><a href="#">Left Nav Button</a></li>
+			</ul>
+		</section>
+	</nav>
+
 	<div class="small-6 columns">
 		<a href="#" class="button expand">BOTAO</a>
 	</div>
