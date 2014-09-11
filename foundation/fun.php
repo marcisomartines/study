535e509567ca89404e49d7ab45cad9db850ff5c4
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-09-11 15:05:08 -0400

    foundation teste

---
+++ foundation/fun.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Teste com o fundation</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<script src="js/foundation.min.js"></script>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="css/foundation.min.css">
+</head>
+<body>
+	<div class="small-6 columns">
+		<a href="#" class="button expand">BOTAO</a>
+	</div>
+	<div class="small-6 columns">
+		<a href="#" class="button success expand">SUCESSO</a>
+	</div>
+	<div class="small-6 columns">
+		<a href="#" class="button radius expand">Canto arredondado</a>
+	</div>
+	<div class="small-6 columns">
+		<a href="#" class="button radius alert expand">Canto arredondado</a>
+	</div>
+	<div class="small-6 columns">
+		<a href="#" class="button round alert expand">Canto arredondado</a>
+	</div>
+	<div class="small-6 columns">
+		<a href="#" class="button round success expand">Canto arredondado</a>
+	</div>
+</body>
+</html>
