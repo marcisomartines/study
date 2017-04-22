c9943d8225a6d56b23a0a77351b7ab701a5624d2
marciso.gonzalez@gmail.com <marciso.gonzalez@gmail.com>
2017-04-22 19:45:00 -0400

    php7

---
+++ php7_testes/nullable.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marciso
+ * Date: 22/04/2017
+ * Time: 19:32
+ */
+
+function teste(?int $x){
+    return $x;
+}
+
+var_dump(teste(NULL));
---
+++ php7_testes/returno_void.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marciso
+ * Date: 22/04/2017
+ * Time: 19:43
+ */
+
+function testando($x):void{
+    echo $x;
+    return;
+}
+
+echo testando('testo da função');
